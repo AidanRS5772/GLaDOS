63d0aefb764f0325ea31f349eb34d0e4ba1571e6
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2024-07-17 13:58:53 -0700

    made code for camera matricies

--- homology.cpp
+++ homology.cpp
@@ -7,17 +7,15 @@
 using namespace std;
 
 Eigen::Matrix3d make_rotation_matrix(const vector<double> &orientation) {
-  // Construct rotation quaternion
   Eigen::Quaterniond quaternion =
       Eigen::AngleAxisd(orientation[2] * (2 * M_PI), Eigen::Vector3d::UnitZ()) *
       Eigen::AngleAxisd(orientation[1] * (M_PI), Eigen::Vector3d::UnitY()) *
       Eigen::AngleAxisd(orientation[0] * (M_PI / 2), Eigen::Vector3d::UnitX());
 
-  // Convert the quaternion to a rotation matrix
   return quaternion.toRotationMatrix();
 }
 
-tuple<Eigen::Matrix<double, 4, 3>, Eigen::Matrix<double, 4, 3>>
+tuple<Eigen::Matrix<double, 3, 4>, Eigen::Matrix<double, 3, 4>>
 make_camera_matrices() {
   rapidcsv::Document doc("calibrations.csv");
   double f = doc.GetColumn<double>("focal")[0];
@@ -36,22 +34,18 @@ make_camera_matrices() {
   H2.block<3, 3>(0, 0) = R2;
 
   for (int i = 0; i < 3; i++) {
-    H1(3, i) = translation1[i];
-    H2(3, i) = translation2[i];
+    H1(i, 3) = translation1[i];
+    H2(i, 3) = translation2[i];
   }
 
   H1(3, 3) = 1.0;
   H2(3, 3) = 1.0;
 
-  Eigen::Matrix<double, 4, 3> F;
+  Eigen::Matrix<double, 3, 4> F;
   F << f, 0.0, 0.0, 0.0, 0.0, f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0;
 
-  cout << H1 << endl;
-  cout << H2 << endl;
-  cout << F << endl;
-
-  Eigen::Matrix<double, 4, 3> C1 = F * H1;
-  Eigen::Matrix<double, 4, 3> C2 = F * H2;
+  Eigen::Matrix<double, 3, 4> C1 = F * H1;
+  Eigen::Matrix<double, 3, 4> C2 = F * H2;
 
   return make_tuple(C1, C2);
 }
@@ -60,8 +54,8 @@ int main() {
 
   auto [C1, C2] = make_camera_matrices();
 
-  cout << C1 << endl;
-  cout << C2 << endl;
+  cout << "C1:\n" << C1 << endl;
+  cout << "C2:\n" << C2 << endl;
 
   return 0;
 }
