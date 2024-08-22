6f1e321761491f53e0ec369de6979300cb176964
AidanRS5772 <aidanrsgarlato@gmail.com>
2024-08-21 22:02:45 -0700

    implemented simulated facial recognition

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <opencv2/opencv.hpp>
 #include <boost/asio.hpp>
 #include <vector>
+#include <cstdlib>
+#include "homology.h"
+#include <thread>
+#include <atomic>
+#include <chrono>
 
 using boost::asio::ip::tcp;
 using namespace std;
@@ -44,8 +49,16 @@ array<int, 2> find_center_of_max_contour(const vector<std::vector<cv::Point>> &c
 
     array<int, 2> px_cords = {Laser_X, Laser_Y};
 
-    cv::circle(frame, cv::Point(Laser_X, Laser_Y), 5, cv::Scalar(255, 0, 0), -1);
+    cv::circle(frame, cv::Point(Laser_X, Laser_Y), 15, cv::Scalar(0, 0, 255), -1);
     return px_cords;
+}
+
+void recognize_face(atomic<bool> &face_recog_done, atomic<bool> &face_recog){
+    face_recog_done.store(false);
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "Finished Facial Recognition" << endl;
+    face_recog_done.store(true);
+    face_recog.store(true);
 }
 
 const string IP = "10.0.0.235";
@@ -53,25 +66,31 @@ const string PORT = "12345";
 
 const int PRE_THRESH = 400; //Threshold for KNN background subtractor
 const int FRAME_HIST = 100; //Frame History of KNN background subtractor
-
 const int L_KERNAL_SZ = 7; //Size of convolution kernal for large morphology processing
-const int S_KERNAL_SZ = 7; //Size of convolution kernal for large morphology processing
-
-const int POST_THRESH = 50; //Gray scale limit for post thresh
+const int S_KERNAL_SZ = 7; //Size of convolution kernal for small morphology processing
+const int POST_THRESH = 50; //Gray scale limit for post threshholding of mask
 
 
 int main() {
-    int frame_cnt = 0;
-
+    //K Means Subtractor
     cv::Ptr<cv::BackgroundSubtractorKNN> KNN = cv::createBackgroundSubtractorKNN(FRAME_HIST, PRE_THRESH, true);
     cv::Mat frame, fg_mask, clean_fg_mask;
-    vector<std::vector<cv::Point>> contours;
+
+    //Image Clean Up
     cv::Mat kernel_L = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(L_KERNAL_SZ, L_KERNAL_SZ));
     cv::Mat kernel_S = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(S_KERNAL_SZ, S_KERNAL_SZ));
+    
+    vector<std::vector<cv::Point>> contours; // Countours
+    
+    //Facial Recognition
+    atomic<bool> face_recog_done(true);
+    atomic<bool> face_recog(false);
 
-
+    auto start = chrono::steady_clock::now();
+    signed long int timer = 2; //timer to revert back to unrecognized
 
     try {
+        // TCP/WebSocket stuff
         boost::asio::io_context io_context;
         tcp::socket socket(io_context);
         tcp::resolver resolver(io_context);
@@ -80,26 +99,38 @@ int main() {
 
         while (true) {
             try {
+                // get frame from websocket
                 frame = process_data_to_frame(socket);
-                frame_cnt ++;
-                cout << "Processed Frames: " << frame_cnt;
+                
+                KNN->apply(frame, fg_mask); // K Means Motion detection mask
 
-                KNN->apply(frame, fg_mask);
-
+                // Frame Clean Up
                 cv::threshold(fg_mask, clean_fg_mask, POST_THRESH, 255, cv::THRESH_BINARY);
                 cv::morphologyEx(clean_fg_mask, clean_fg_mask, cv::MORPH_OPEN, kernel_S);
                 cv::morphologyEx(clean_fg_mask, clean_fg_mask, cv::MORPH_CLOSE, kernel_L);
 
-                cv::findContours(clean_fg_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-                find_center_of_max_contour(contours, frame);
+                cv::findContours(clean_fg_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE); // Generate Countours
+                if (contours.size() > 0){
+                    find_center_of_max_contour(contours, frame); // find largest countour
+                    if (face_recog_done.load()){
+                        // Spawn off Facial Recognition Process
+                        std::thread recognize_face_process(recognize_face, ref(face_recog_done), ref(face_recog));
+                        recognize_face_process.detach();
+                    }
+                }
+                
+                //check if time has elapsed to switch back to unrecognized face
+                auto now = chrono::steady_clock::now();
+                signed long int elapsed = chrono::duration_cast<chrono::seconds>(now - start).count();
+                if (elapsed >= timer){
+                    face_recog.store(false);
+                }
 
                 cv::imshow("Video Stream", frame);
                 cv::imshow("Background Subtraction", fg_mask);
                 cv::imshow("Background Subtraction And Clean", clean_fg_mask);
 
-                send_acknowledgment(socket);
-
-                cout << flush;
+                send_acknowledgment(socket); // send message that processing is done
 
                 if (cv::waitKey(1) == 'q') {
                     throw std::runtime_error("Stream Ended by User on Client");
