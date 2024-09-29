bc0b03528ec523976d2f0ba4b06ba056b63873b2
AidanRS5772 <aidanrsgarlato@gmail.com>
2024-09-28 18:21:34 -0700

    mod

--- src/client.py
+++ src/client.py
@@ -1,4 +1,3 @@
-import libcamera
 import asyncio
 import websockets
 import cv2
@@ -7,7 +6,7 @@ import threading
 import sys
 import select
 import platform
-import picamera2
+from picamera2 import Picamera2
 
 if platform.system() == "Windows":
     import msvcrt
